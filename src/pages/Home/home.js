b3e28a6637aefa2ecb92405aafa800f2f9b2e1b7
Luboslav Halama <lubo.halama@gmail.com>
2023-02-02 23:01:41 +0100

    feat(cp-stars-frontend): cp stars basic info is loaded into table

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,26 @@
-import InputForm from './inputForm';
+import BasicInfoStarsTable from './Stars';
+import { getBasicInfoForAllStars } from '../../services/DatabaseService';
+import { useEffect, useState } from 'react';
+import { CircularProgress } from '@mui/material';
 
 const Home = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [stars, setStars] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    console.log('starting query');
+    getBasicInfoForAllStars().then((data) => {
+      setStars(data);
+      setLoading(false);
+    });
+    console.log('Query done');
+  }, []);
+
   return (
     <div className="center">
-      <InputForm />
+      {isLoading && <CircularProgress />}
+      {stars && <BasicInfoStarsTable originalRows={stars} />}
     </div>
   );
 };
---
+++ src/shared/databaseURLs.js
@@ -0,0 +1,5 @@
+// const DATABASE_BASE_URL = 'http://localhost:8081/';
+
+export const databaseURLs = {
+  getBasicInfoForAllStars: '/stars',
+};
